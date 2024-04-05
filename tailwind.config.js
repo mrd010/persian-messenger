9a4e93b69620c2c97c9bec11d5f3a221dfd43a29
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-05 14:48:57 +0330

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+import colors from 'tailwindcss/colors';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      theme: colors.violet,
+      gray: colors.slate,
+      dark: '#151923',
+    },
     extend: {},
   },
   plugins: [],
