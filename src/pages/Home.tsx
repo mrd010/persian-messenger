1ae4bca703f30af3c50c85d34b36d8f9b32daa34
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-05 16:29:37 +0330

    Add styles fpr welcome page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+
+type statusTypes = 'welcome' | 'show-login';
+
 export const Home = () => {
+  const [status, setStatus] = useState<statusTypes>('welcome');
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full p-2 overflow-hidden">
       <div className="grid grid-rows-2 justify-center h-full">
         <div className="self-end h-full">
           <img src="/icon.png" className="h-full object-contain" />
         </div>
         <div className="self-start grid grid-rows-2 h-full items-start justify-center">
-          <h1 className="font-Yekan font-bold text-4xl text-center">پیام رسان فارسی</h1>
-          <button className="bg-gray-50 font-Yekan py-2 text-gray-900 rounded-full text-xl font-bold ">
-            <span className="pb-1 inline-block">ورود</span>
-          </button>
+          <h1 className="font-Yekan font-bold text-4xl sm:text-5xl text-center">پیام رسان فارسی</h1>
+          <div className="relative grid">
+            <div
+              className={`bg-gray-50 w-full h-full rounded-full absolute transition-transform will-change-transform duration-700 ${status === 'show-login' ? 'scale-x-[1000%] scale-y-[4000%]' : ''}`}
+            ></div>
+            <button
+              className={`font-Yekan py-2 z-20 text-gray-950 will-change-transform rounded-full text-xl sm:text-2xl font-bold transition-transform ${status === 'welcome' ? 'scale-100' : 'scale-0'}`}
+              onClick={() => setStatus('show-login')}
+            >
+              ورود
+            </button>
+          </div>
         </div>
       </div>
     </div>
