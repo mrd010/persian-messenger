d52fb5a94e6549c0c56177bf8071851d7cab9422
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-05 15:56:52 +0330

    Add home page elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen w-full">
+      <div className="grid grid-rows-2 justify-center h-full">
+        <div className="self-end h-full">
+          <img src="/icon.png" className="h-full object-contain" />
+        </div>
+        <div className="self-start grid grid-rows-2 h-full items-start justify-center">
+          <h1 className="font-Yekan font-bold text-4xl text-center">پیام رسان فارسی</h1>
+          <button className="bg-gray-50 font-Yekan py-2 text-gray-900 rounded-full text-xl font-bold ">
+            <span className="pb-1 inline-block">ورود</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
