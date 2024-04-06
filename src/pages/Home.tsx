17cf3b4cbefefd67bcabb73320bc1203f6a14371
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-06 22:32:41 +0330

    Get login page outside of Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
-import { LoginForm } from '../components/LoginForm';
 
-type statusTypes = 'welcome' | 'show-login';
+type HomeProps = {
+  onLoginClick: () => void;
+};
 
-export const Home = () => {
+export const Home = ({ onLoginClick }: HomeProps) => {
   // home page status
-  const [status, setStatus] = useState<statusTypes>('welcome');
+  const [isLoginPressed, setIsLoginPressed] = useState(false);
+
+  const handleShowLogin = () => {
+    setIsLoginPressed(true);
+    setTimeout(() => {
+      onLoginClick();
+    }, 500);
+  };
+
   return (
     <div className="h-screen w-full p-2 overflow-hidden">
       {/* container */}
@@ -23,20 +32,17 @@ export const Home = () => {
             scales to fullscreen when click login button
              */}
             <div
-              className={`bg-gray-50 w-full h-full rounded-full absolute transition-transform will-change-transform duration-700 ${status === 'show-login' ? 'scale-x-[1000%] scale-y-[4000%]' : ''}`}
+              className={`bg-gray-50 w-full h-full rounded-full absolute transition-transform will-change-transform duration-700 ${isLoginPressed ? 'scale-x-[1000%] scale-y-[4000%]' : ''}`}
             ></div>
             {/* login button */}
             <button
-              className={`font-Yekan py-2 z-20 text-gray-950 will-change-transform rounded-full text-xl sm:text-2xl font-bold transition-transform ${status === 'welcome' ? 'scale-100' : 'scale-0'}`}
-              onClick={() => setStatus('show-login')}
+              className={`font-Yekan py-2 z-20 text-gray-950 will-change-transform rounded-full text-xl sm:text-2xl font-bold transition-transform ${isLoginPressed ? 'scale-0' : 'scale-100'}`}
+              onClick={handleShowLogin}
             >
               ورود
             </button>
           </div>
         </div>
-      </div>
-      <div className="fixed top-0 left-0 size-full p-6 grid items-center">
-        <LoginForm></LoginForm>
       </div>
     </div>
   );
