530d50873614ea0d44d66ba982a82126adeac9c7
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-06 22:34:32 +0330

    Make app some pages based on status

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+
+type Pages = 'home' | 'login';
 
 export const App = () => {
+  const [currentPage, setCurrentPage] = useState<Pages>('home');
   return (
-    <div className="bg-gray-950 text-gray-50 font-Samim">
-      <Home></Home>
+    <div className="bg-gray-950 text-gray-50 font-Samim" dir="rtl">
+      {currentPage === 'home' && <Home onLoginClick={() => setCurrentPage('login')}></Home>}
+      {currentPage === 'login' && <Login></Login>}
     </div>
   );
 };
