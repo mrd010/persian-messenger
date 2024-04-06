ca9480c1219c64c907dc1899e31dff78fb2e3f38
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-07 01:03:59 +0330

    Add get users feature

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,63 @@
 import { useEffect, useState } from 'react';
 import { GetPhone } from '../components/GetPhone';
+import { useData } from '../hooks/useData';
+import { faravin } from '../data/apiUrls';
+
+interface UserType {
+  username: string;
+  password: string;
+  name: string;
+  date: string;
+}
+
+interface AllUsersType {
+  code: string;
+  message: string;
+  userList: UserType[];
+}
 
 type LoginPageStatus = '' | 'input-phone';
 
 export const Login = () => {
   const [status, setStatus] = useState<LoginPageStatus>('');
   const [userPhoneNumber, setUserPhoneNumber] = useState('');
+  const [loginAttempts, setLoginAttempts] = useState(0);
 
+  const dataFetchDeps = JSON.stringify([loginAttempts, userPhoneNumber]);
+  // get all users when attempt login
+  const { data, isError, isLoading } = useData<AllUsersType>(faravin.usersAll, dataFetchDeps);
+  console.log(dataFetchDeps);
+
+  // when a valid phone number submitted
   const handleGetPhoneNumber = (phoneNumber: string) => {
     setUserPhoneNumber(phoneNumber);
+    setLoginAttempts((a) => a + 1);
   };
 
+  // after a while of bg appear show form
   useEffect(() => {
     setTimeout(() => {
       setStatus('input-phone');
     }, 50);
   });
+
   return (
     <div className="fixed size-full p-4 bg-gray-50">
-      <GetPhone hidden={status === ''}></GetPhone>
+      {isLoading && (
+        <div className="size-full fixed top-0 left-0 z-10">
+          <div className="loader w-full absolute bottom-2"></div>
+        </div>
+      )}
+      <div
+        className={`absolute w-full left-0 text-center bottom-4 transition-opacity ${isError ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <p className="bg-error-600 rounded-md w-max mx-auto p-2">خطا در برقراری ارتباط با سرور</p>
+      </div>
+      <GetPhone
+        hidden={status === ''}
+        isLoadingData={isLoading}
+        onValidSubmit={handleGetPhoneNumber}
+      ></GetPhone>
     </div>
   );
 };
