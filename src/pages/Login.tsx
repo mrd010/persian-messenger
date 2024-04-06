cf2eae792b5700d8f8ac557cf2117cff80451556
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-07 02:23:11 +0330

    Move fetch data into submit acton

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,63 +1,71 @@
 import { useEffect, useState } from 'react';
 import { GetPhone } from '../components/GetPhone';
-import { useData } from '../hooks/useData';
-import { faravin } from '../data/apiUrls';
+import { getAllUsers } from '../utilities/fetchers';
+import { LoginForm } from '../components/LoginForm';
 
-interface UserType {
-  username: string;
-  password: string;
-  name: string;
-  date: string;
-}
-
-interface AllUsersType {
-  code: string;
-  message: string;
-  userList: UserType[];
-}
-
-type LoginPageStatus = '' | 'input-phone';
+type LoginPageStatus = '' | 'login-phone' | 'login-password' | 'sign-up';
 
 export const Login = () => {
   const [status, setStatus] = useState<LoginPageStatus>('');
-  const [userPhoneNumber, setUserPhoneNumber] = useState('');
-  const [loginAttempts, setLoginAttempts] = useState(0);
+  // const [userPhoneNumber, setUserPhoneNumber] = useState('');
+  const [isError, setIsError] = useState(false);
 
-  const dataFetchDeps = JSON.stringify([loginAttempts, userPhoneNumber]);
+  console.log(status);
+  // const [loginAttempts, setLoginAttempts] = useState(0);
+
+  // const dataFetchDeps = JSON.stringify([loginAttempts, userPhoneNumber]);
   // get all users when attempt login
-  const { data, isError, isLoading } = useData<AllUsersType>(faravin.usersAll, dataFetchDeps);
-  console.log(dataFetchDeps);
+  // const { data, isError, isLoading } = useData<AllUsersType>(faravin.usersAll, dataFetchDeps);
+  // console.log(data);
 
   // when a valid phone number submitted
-  const handleGetPhoneNumber = (phoneNumber: string) => {
-    setUserPhoneNumber(phoneNumber);
-    setLoginAttempts((a) => a + 1);
+  const handleGetPhoneNumber = async (phoneNumber: string) => {
+    try {
+      // get all users
+
+      const users = await getAllUsers();
+
+      const userNames = users.map((user) => user.username);
+      const userExists = userNames.includes(phoneNumber);
+
+      console.log(userNames);
+
+      if (userExists) {
+        setStatus('login-password');
+      } else {
+        setStatus('sign-up');
+      }
+    } catch (error) {
+      setIsError(true);
+    }
   };
 
   // after a while of bg appear show form
   useEffect(() => {
     setTimeout(() => {
-      setStatus('input-phone');
+      setStatus('login-phone');
     }, 50);
-  });
+  }, []);
+
+  // reset error
+  useEffect(() => {
+    if (isError) {
+      setTimeout(() => {
+        setIsError(false);
+      }, 3000);
+    }
+  }, [isError]);
 
   return (
     <div className="fixed size-full p-4 bg-gray-50">
-      {isLoading && (
-        <div className="size-full fixed top-0 left-0 z-10">
-          <div className="loader w-full absolute bottom-2"></div>
-        </div>
-      )}
       <div
         className={`absolute w-full left-0 text-center bottom-4 transition-opacity ${isError ? 'opacity-100' : 'opacity-0'}`}
       >
         <p className="bg-error-600 rounded-md w-max mx-auto p-2">خطا در برقراری ارتباط با سرور</p>
       </div>
-      <GetPhone
-        hidden={status === ''}
-        isLoadingData={isLoading}
-        onValidSubmit={handleGetPhoneNumber}
-      ></GetPhone>
+      <LoginForm hidden={status !== 'login-phone'}>
+        <GetPhone onValidSubmit={handleGetPhoneNumber}></GetPhone>
+      </LoginForm>
     </div>
   );
 };
