b173329f5705dd24bf630e64d87f07278fddcb61
mrd010win <126393669+mrd010@users.noreply.github.com>
2024-04-07 13:09:05 +0330

    Use new hooks and components instead

--- src/components/GetPhone.tsx
+++ src/components/GetPhone.tsx
@@ -1,40 +1,19 @@
-import { useState } from 'react';
 import { InputField } from './InputField';
+import { usePhoneNumber } from '../hooks/usePhoneNumber';
+import { PhoneInput } from './PhoneInput';
+import { FormSubmitButton } from './FormSubmitButton';
 
 type GetPhoneProps = {
   onValidSubmit: (validPhoneNumber: string) => void;
 };
 
 export const GetPhone = ({ onValidSubmit }: GetPhoneProps) => {
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [isValid, setIsValid] = useState(true);
-  // error for submit
-
-  // check if input value is ok for type
-  const isValueOkToInput = (value: string): boolean => {
-    return (/^\d*$/.test(value) && value.length < 12) || value.length === 0;
-  };
-
-  //   check if input value is a phone number
-  const isValidPhoneNumber = (value: string): boolean => {
-    return /^09([0-9]{9})$/.test(value);
-  };
-
-  //   phone number input controller
-  const handleUpdatePhone = (value: string) => {
-    // check if input is correct for number then update value
-    if (isValueOkToInput(value)) {
-      setIsValid(isValidPhoneNumber(value));
-      setPhoneNumber(value);
-    }
-  };
+  const { phoneNumber, isValid, updatePhoneNumber } = usePhoneNumber();
 
   // when submit button pressed if phone number is valid proceed else show error to user
   const handleSubmitPhoneNumber = () => {
-    if (isValidPhoneNumber(phoneNumber)) {
+    if (isValid) {
       onValidSubmit(phoneNumber);
-    } else {
-      setIsValid(false);
     }
   };
 
@@ -44,28 +23,13 @@ export const GetPhone = ({ onValidSubmit }: GetPhoneProps) => {
         لطفا شماره موبایل خود را وارد نمایید.
       </p>
       <form action="" className="flex flex-col flex-nowrap gap-4">
-        <InputField
-          name="phoneNumber"
-          labelText="شماره موبایل"
-          type="tel"
-          value={phoneNumber}
-          isValid={isValid}
-          onInput={handleUpdatePhone}
-        ></InputField>
-        {/* submit button
-        enabled only when phone number is valid
-        */}
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleSubmitPhoneNumber();
-          }}
-          className="text-theme-50 grid place-items-center rounded-full py-2 bg-gradient-to-tr from-theme-500 to-theme-700 shadow-sm font-bold transition-opacity disabled:opacity-25 disabled:from-gray-500/80 disabled:to-gray-700/80"
-          disabled={!isValid}
-        >
-          ورود
-        </button>
+        <PhoneInput isValid={isValid} value={phoneNumber} onUpdate={updatePhoneNumber}></PhoneInput>
+        {/* submit button. enabled only when phone number is valid
+         */}
+        <FormSubmitButton
+          formIsValid={isValid}
+          onSubmit={handleSubmitPhoneNumber}
+        ></FormSubmitButton>
       </form>
       <div className="flex flex-col p-4 gap-2">
         <button className="text-theme-500 font-Yekan text-right">
